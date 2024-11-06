b7a4581611cf949e78f485f1e6451e8ed2ec7c85
Omgshit <123563422+Omgshit@users.noreply.github.com>
2024-11-06 12:30:51 -0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -26,6 +26,9 @@ elseif game.PlaceId == 13601434903 then
 elseif game.PlaceId == 94801184757522 then
     print ("Piecs jujutusu duc")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/refs/heads/main/Jujutsu%20piece"))()
+elseif game.PlaceId == 89438510123061 then
+    print ("Shadow Is Emergy")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/refs/heads/main/AnimeShadow"))()
 elseif game.PlaceId == 18337464872 or game.PlaceId == 114252638377752 then
     print ("Weak Legacy")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/main/Weak%20Legacy%202"))()
