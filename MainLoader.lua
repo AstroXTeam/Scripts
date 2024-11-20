76528ee6a1466de2ce368afb62e15501751a3058
Omgshit <123563422+Omgshit@users.noreply.github.com>
2024-11-20 00:49:21 -0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -26,7 +26,7 @@ elseif game.PlaceId == 13601434903 then
 elseif game.PlaceId == 94801184757522 then
     print ("Piecs jujutusu duc")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/refs/heads/main/Jujutsu%20piece"))()
-elseif game.PlaceId == 19001778364 or game.PlaceId == 107410502434648 then
+elseif game.PlaceId == 19001778364 or game.PlaceId == 107410502434648 or game.PlaceId == 17083240635 then
     print ("Slayer Online")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/refs/heads/main/SlayerOnline"))()
 elseif game.PlaceId == 89438510123061 then
