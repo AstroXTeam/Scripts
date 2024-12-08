b6c1e845354e45fee2f47f6b31aaf7284998df3d
Omgshit <123563422+Omgshit@users.noreply.github.com>
2024-12-08 14:01:35 -0300

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -29,7 +29,7 @@ elseif game.PlaceId == 94801184757522 then
 elseif game.PlaceId == 19001778364 or game.PlaceId == 107410502434648 or game.PlaceId == 17083240635 then
     print ("Slayer Online")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/refs/heads/main/SlayerOnline"))()
-elseif game.PlaceId == 100222912269336 then
+elseif game.PlaceId == 100222912269336 or game.PlaceId == 84188796720288 then
     print ("Anime Realms") 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Omgshit/Scripts/refs/heads/main/AnimeRealms"))()
 elseif game.PlaceId == 89438510123061 then
